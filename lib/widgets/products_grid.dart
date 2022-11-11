8231719c1f1911fa000820ae34cac8e18a1a766a
Fernando Nellmeldin <fernando.nellmeldin@gmail.com>
2022-11-11 14:31:08 +0100

    handle favorite status with another provider

--- lib/widgets/products_grid.dart
+++ lib/widgets/products_grid.dart
@@ -1,30 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-import '../models/product.dart';
+import '../providers/product.dart';
 import '../widgets/product_item.dart';
 import '../providers/products.dart';
+import 'package:provider/provider.dart';
 
 class ProductsGrid extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    final productsData = Provider.of<Products>(context);
+    final productsData = Provider.of<Products>(context); // listen to provider
     final products = productsData.items;
     return GridView.builder(
-        padding: const EdgeInsets.all(10.0),
-        itemCount: products.length,
-        itemBuilder: (ctx, index) => ProductItem(
-          products[index].id,
-          products[index].title,
-          products[index].imageUrl,
-        ),
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          childAspectRatio: 3 / 2,
-          crossAxisSpacing: 10,
-          mainAxisSpacing: 10,
-        ),
-      );
+      padding: const EdgeInsets.all(10.0),
+      itemCount: products.length,
+      itemBuilder: (ctx, index) => ChangeNotifierProvider(
+        create: (c) => products[index],
+        child: ProductItem(),
+      ),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+      ),
+    );
   }
 }
