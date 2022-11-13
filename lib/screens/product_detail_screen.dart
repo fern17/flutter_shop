8b4f4e8b15d84bee30d6300f09a7395d8fdf8600
Fernando Nellmeldin <fernando.nellmeldin@gmail.com>
2022-11-13 20:27:30 +0100

    fill info in details screen

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -14,12 +14,51 @@ class ProductDetailScreen extends StatelessWidget {
     final productId = ModalRoute.of(context)!.settings.arguments as String;
     final loadedProduct = Provider.of<Products>(
       context,
-      listen: false, // avoid to rerun when Product object changes, runs only once!
+      listen:
+          false, // avoid to rerun when Product object changes, runs only once!
     ).findById(productId);
     return Scaffold(
       appBar: AppBar(
         title: Text(
           loadedProduct.title,
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              width: double.infinity,
+              height: 300,
+              child: Image.network(
+                loadedProduct.imageUrl,
+                fit: BoxFit.cover,
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Text(
+              '\$${loadedProduct.price}',
+              style: const TextStyle(
+                color: Colors.grey,
+                fontSize: 20,
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Container(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 10,
+              ),
+              width: double.infinity,
+              child: Text(
+                loadedProduct.description,
+                textAlign: TextAlign.center,
+                softWrap: true,
+              ),
+            ),
+          ],
         ),
       ),
     );
