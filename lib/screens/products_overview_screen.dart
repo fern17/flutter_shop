78c5ac88ecad8582fdd8c275c1138755f337d66e
Fernando Nellmeldin <fernando.nellmeldin@gmail.com>
2022-11-13 21:13:16 +0100

    add a drawer to go between shop and orders

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -2,11 +2,14 @@ import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter/material.dart';
+
 import '../widgets/products_grid.dart';
 import '../providers/products.dart';
 import '../screens/cart_screen.dart';
 import '../widgets/badge.dart';
 import '../providers/cart.dart';
+import '../widgets/app_drawer.dart';
+
 enum FilterOptions {
   Favorites,
   All,
@@ -22,42 +25,46 @@ class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('My Shop'), actions: <Widget>[
-        PopupMenuButton(
-          onSelected: (FilterOptions selectedValue) {
-            setState(() {
-              _showOnlyFavorites = (selectedValue == FilterOptions.Favorites);
-            });
-          },
-          icon: Icon(
-            Icons.more_vert,
-          ),
-          itemBuilder: (_) => [
-            PopupMenuItem(
-              child: Text('Only favorites'),
-              value: FilterOptions.Favorites,
-            ),
-            PopupMenuItem(
-              child: Text('Show all'),
-              value: FilterOptions.All,
-            ),
-          ],
-        ),
-        Consumer<Cart>(
-          builder: (_, cartObject, ch) => Badge(
-            child: ch!,
-            value: cartObject.itemCount.toString(),
-          ),
-          child: IconButton(
-            icon: Icon(
-              Icons.shopping_cart,
-            ),
-            onPressed: () {
-              Navigator.of(context).pushNamed(CartScreen.routeName);
+      appBar: AppBar(
+        title: Text('My Shop'),
+        actions: <Widget>[
+          PopupMenuButton(
+            onSelected: (FilterOptions selectedValue) {
+              setState(() {
+                _showOnlyFavorites = (selectedValue == FilterOptions.Favorites);
+              });
             },
+            icon: Icon(
+              Icons.more_vert,
+            ),
+            itemBuilder: (_) => [
+              PopupMenuItem(
+                child: Text('Only favorites'),
+                value: FilterOptions.Favorites,
+              ),
+              PopupMenuItem(
+                child: Text('Show all'),
+                value: FilterOptions.All,
+              ),
+            ],
           ),
-        ), 
-      ]),
+          Consumer<Cart>(
+            builder: (_, cartObject, ch) => Badge(
+              child: ch!,
+              value: cartObject.itemCount.toString(),
+            ),
+            child: IconButton(
+              icon: Icon(
+                Icons.shopping_cart,
+              ),
+              onPressed: () {
+                Navigator.of(context).pushNamed(CartScreen.routeName);
+              },
+            ),
+          ),
+        ],
+      ),
+      drawer: AppDrawer(),
       body: ProductsGrid(_showOnlyFavorites),
     );
   }
