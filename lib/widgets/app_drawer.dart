7048857adbbcb075be415e70f2cb5c13b40981d6
Fernando Nellmeldin <fernando.nellmeldin@gmail.com>
2022-11-22 20:12:23 +0100

    add a screen for user products

--- lib/widgets/app_drawer.dart
+++ lib/widgets/app_drawer.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../screens/orders_screen.dart';
+import '../screens/user_products_screen.dart';
 
 class AppDrawer extends StatelessWidget {
   @override
@@ -8,25 +9,35 @@ class AppDrawer extends StatelessWidget {
       child: Column(
         children: [
           AppBar(
-            title: Text('Hello Friend!'),
+            title: const Text('Hello Friend!'),
             automaticallyImplyLeading: false,
           ),
-          Divider(),
+          const Divider(),
           ListTile(
-            leading: Icon(Icons.shop),
-            title: Text(
+            leading: const Icon(Icons.shop),
+            title: const Text(
               'Shop',
             ),
             onTap: () => Navigator.of(context).pushReplacementNamed('/'),
           ),
-          Divider(),
+          const Divider(),
           ListTile(
-            leading: Icon(Icons.shop),
-            title: Text(
+            leading: const Icon(Icons.shop),
+            title: const Text(
               'Orders',
             ),
             onTap: () => Navigator.of(context).pushReplacementNamed(
               OrdersScreen.routeName,
+            ),
+          ),
+          const Divider(),
+          ListTile(
+            leading: const Icon(Icons.edit),
+            title: const Text(
+              'Manage Products',
+            ),
+            onTap: () => Navigator.of(context).pushReplacementNamed(
+              UserProductsScreen.routeName,
             ),
           ),
         ],
