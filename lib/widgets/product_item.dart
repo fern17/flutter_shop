9783f23fe175951e238c9ed2e9d171ee449dbf32
Fernando Nellmeldin <fernando.nellmeldin@gmail.com>
2022-11-22 19:45:09 +0100

    get rid of deprecation warnings

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -42,13 +42,13 @@ class ProductItem extends StatelessWidget {
               color: Theme.of(context).accentColor,
               onPressed: () {
                 cart.addItem(product.id, product.price, product.title);
-                Scaffold.of(context).hideCurrentSnackBar();
-                Scaffold.of(context).showSnackBar(
+                ScaffoldMessenger.of(context).hideCurrentSnackBar();
+                ScaffoldMessenger.of(context).showSnackBar(
                   SnackBar(
-                    content: Text(
+                    content: const Text(
                       'Added item to cart',
                     ),
-                    duration: Duration(
+                    duration: const Duration(
                       seconds: 2,
                     ),
                     action: SnackBarAction(
